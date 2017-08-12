fdbf5c5e27ee10a3098d4f359d617ad1b8f65585
itchrand <ac_is_nowhere@yahoo.com>
2017-08-12 19:50:37 +0200

    release 0.1 added team B, reset, style

--- app/src/main/java/it/seahawk/courtcounter/MainActivity.kt
+++ app/src/main/java/it/seahawk/courtcounter/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     var scoreTeamA: Int = 0
+    var scoreTeamB: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +21,13 @@ class MainActivity : AppCompatActivity() {
      */
     fun displayScoreForTeamA(score: Int) {
         val scoreView = findViewById(R.id.team_a_score) as TextView
+        scoreView.text = score.toString()
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    fun displayScoreForTeamB(score: Int) {
+        val scoreView = findViewById(R.id.team_b_score) as TextView
         scoreView.text = score.toString()
     }
 
@@ -46,5 +54,36 @@ class MainActivity : AppCompatActivity() {
         scoreTeamA = scoreTeamA + 1
         displayScoreForTeamA(scoreTeamA)
     }
+    /**
+     * Add 3 points to score of team B.
+     */
+    fun add3PointsForTeamB(view: View) {
+        scoreTeamB = scoreTeamB + 3
+        displayScoreForTeamB(scoreTeamB)
+    }
 
+    /**
+     * Add 2 points to score of team B.
+     */
+    fun add2PointsForTeamB(view: View) {
+        scoreTeamB = scoreTeamB + 2
+        displayScoreForTeamB(scoreTeamB)
+    }
+
+    /**
+     * Add 1 free throw point to score of team B.
+     */
+    fun addFreeThrowForTeamB(view: View) {
+        scoreTeamB = scoreTeamB + 1
+        displayScoreForTeamB(scoreTeamB)
+    }
+    /**
+     * Reset scores for Team A and Team B.
+     */
+    fun resetScores(view: View) {
+        scoreTeamA = 0
+        scoreTeamB = 0
+        displayScoreForTeamA(scoreTeamA)
+        displayScoreForTeamB(scoreTeamB)
+    }
 }
